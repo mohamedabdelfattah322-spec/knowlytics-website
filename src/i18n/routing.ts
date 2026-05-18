04a028c042c9fe5b64ee5ad0eedc38ddbd1afdb1
Knowlytics Hub <admin@knowlytics.com>
2026-05-18 12:39:53 +0300

    fix: routing for Netlify deployment - set ar as default locale

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,6 +2,6 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["en", "ar"],
-  defaultLocale: "en",
-  localePrefix: "always",
+  defaultLocale: "ar",
+  localePrefix: "as-needed",
 });
